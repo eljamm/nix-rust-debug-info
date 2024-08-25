c83b95c656281bca748ca35c6142fe2961e3a730
eljamm <fedi.jamoussi@protonmail.ch>
2024-08-25 16:32:43 +0100

    compile naja

--- show-source-info.sh
+++ show-source-info.sh
@@ -10,30 +10,8 @@ main() {
     --print-build-logs
   )
 
-  if [[ -d ~/git/nixpkgs ]]; then
-    args+=(--override-input nixpkgs ~/git/nixpkgs)
-  else
-    nix flake update nixpkgs
-  fi
+  rm -f ./result-naja
 
-  rm -f ./result-debug ./result-release ./result-sysroot-debug ./result-sysroot-release
-
-  if [[ "$(uname -s)" != Darwin ]]; then
-    nix build .#sysroot-debug --out-link ./result-sysroot-debug "${args[@]}"
-    nix build .#sysroot-release --out-link ./result-sysroot-release "${args[@]}"
-  fi
-
-  nix build .#debug --out-link ./result-debug "${args[@]}"
-  nix build .#release --option sandbox false --out-link ./result-release "${args[@]}"
-
-  nix shell nixpkgs#rustc nixpkgs#lldb -c \
-    rust-lldb result-debug/bin/nix-rust-debug-info \
-    --batch \
-    -o 'break set --name nix_rust_debug_info::a_function' \
-    -o 'process launch' \
-    -o 'source info' ||
-    true
-
-  find . -maxdepth 1 -name 'result-*' -exec ls -A {}/bin \;
+  nix build .#naja --out-link ./result-naja "${args[@]}"
 }
 main "$@"

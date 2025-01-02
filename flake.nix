509228e598123b4b1bfc544b82ffa0e3c0234a2e
eljamm <fedi.jamoussi@protonmail.ch>
2025-01-02 10:40:38 +0100

    build voicevox aarch64

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   # NOTE: I really need to improve this whole workflow, but this works well
   # enough for now, I guess
-  inputs.nixpkgs.url = "github:TomaSajt/nixpkgs/voicevox";
+  inputs.nixpkgs.url = "github:eljamm/nixpkgs/voicevox-test";
   outputs =
     { nixpkgs, ... }:
     let
